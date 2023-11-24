921f67b905c174067929c6fc2388775d4fed7faa
JoeArias1121 <joearias1121@gmail.com>
2023-11-24 17:33:54 -0500

    Adding local storage to save to do list on session end

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const tasks = document.getElementsByClassName("task");
 console.log(tasks);
+console.log("hey")
+const store = 7;
+
+const sdnj = () => {
+    for(let i = 0;i<tasks.length;i++){
+        tasks[i].addEventListener("change", () => {
+            localStorage.setItem(`${i}`,JSON.stringify(tasks[i].value));
+            //console.log(localStorage.getItem(i));
+        });
+    }
+    console.log("working");
+};
+
+sdnj();
+
+for(let i = 0; i<tasks.length;i++) {
+    //tasks[i].value = JSON.parse(localStorage.getItem(i));
+}
+
+sessionStorage.setItem("myStorage", "finally");
